f799bd901f856718519b0903ea3e593caa193a0b
yanezreed <yanezreed@gmail.com>
2025-10-30 01:15:46 +0000

    Fix: call super().__init__() in MainWindow

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import sys
 
 # Following course example...
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+        button = QPushButton("Click Me")
+        self.setCentralWidget(button)
+
 app = QApplication(sys.argv)
-
-window = QMainWindow()
-window.setWindowTitle("My App")
-
-button = QPushButton("Click Me")
-window.setCentralWidget(button)
+window = MainWindow()
 window.show()
-
 app.exec()
+
+# Structure: app -> window -> button
